0e53329c230e878da6c7c9c7e5b6360711329e97
cafehaine <kilian.guillaume@gmail.com>
2021-01-24 19:34:50 +0100

    Actually take screenshots
    Closes #1

--- screenshot_script.sh
+++ screenshot_script.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 sleep 5 # wait for ui to finalize starting up
-# If running on xorg: scrot
-# Otherwise, probably compatible with grim
-touch /vagrant/screenshots/$DE_NAME # TODO take screenshot
+scrot "/vagrant/screenshots/${DE_NAME}.png" || grim "/vagrant/screenshots/${DE_NAME}.png"
 echo "OK" | nc -nc 127.0.0.1 1042
