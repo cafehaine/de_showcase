dcf4d9293e0c84be35c387e983035f1c8c3766fc
cafehaine <kilian.guillaume@gmail.com>
2021-01-29 09:40:39 +0100

    Skip inital setup for Deepin
    Updates #9

--- demodules/deepin.py
+++ demodules/deepin.py
@@ -1,3 +1,5 @@
+import os
+
 from demodules import register_module, BaseModule
 
 
@@ -7,3 +9,18 @@ class Deepin(BaseModule):
     dependencies = ["deepin", "deepin-extra"]
     startup_cmd = "/usr/bin/startdde"
     has_autostart = False
+
+    def start(self):
+        os.makedirs("/home/vagrant/.config/deepin/deepin-wm-switcher")
+        with open(
+            "/home/vagrant/.config/deepin/deepin-wm-switcher/config.json", "w"
+        ) as config_file:
+            config_file.write(
+                """
+            {
+                "allow_switch": true,
+                "last_wm": "deepin-metacity"
+            }
+            """
+            )
+        super().start()
